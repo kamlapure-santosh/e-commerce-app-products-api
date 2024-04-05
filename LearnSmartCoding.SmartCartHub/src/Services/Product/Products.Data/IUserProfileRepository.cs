d8b504b83835839bb674c9ccdb949c1ea61d316c
Santosh Kamlapure <122056114+kamlapure-santosh@users.noreply.github.com>
2024-04-06 01:20:20 +0530

    First Commit

---
+++ LearnSmartCoding.SmartCartHub/src/Services/Product/Products.Data/IUserProfileRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Products.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Products.Data
+{
+    public  interface IUserProfileRepository
+    {
+        Task<UserProfile?> GetUserProfileAsync(string adObjId);
+    }
+    public class UserProfileRepository : IUserProfileRepository
+    {
+        private readonly skCartHubDbContext _dbContext;
+
+        public UserProfileRepository(skCartHubDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public Task<UserProfile?> GetUserProfileAsync(string adObjId)
+        {
+            return _dbContext.UserProfiles.FirstOrDefaultAsync(propa => propa.AdObjId == adObjId);
+        }
+    }
+}
